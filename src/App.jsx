0f700ac35dada88f157c090a16fc9279344df00a
hajin-park <hajin1819@gmail.com>
2024-01-08 18:41:41 -0800

    Added boiler plate structure files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-    return <h1>Based Math Game</h1>;
+    return (
+        <h1 className="font-black text-4xl w-full h-full text-center">
+            Based Math Game
+        </h1>
+    );
 }
 
 export default App;
